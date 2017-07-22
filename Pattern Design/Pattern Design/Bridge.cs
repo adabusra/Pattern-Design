6e533e7c524f9fb126aa01d6e03e6c2e0eaac226
adabusra <busraa.adaa@gmail.com>
2017-07-22 22:19:34 +0300

    Bridge one

---
+++ Pattern Design/Pattern Design/Bridge.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Design
+{
+    class bridge
+    {
+        static void Main()
+        {
+            Abstraction ab = new RefinedAbstraction();
+
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+
+
+
+            Console.ReadKey();
+       
+        }
+    }
+
+    class Abstraction
+    {
+        protected Implementor implementor;
+        
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    abstract class Implementor
+    {
+        public abstract void Operation();
+
+    }
+    class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+
+    }
+
+    class ConcreteImplementorA: Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorA");
+        }
+
+    }
+    
+    class ConcreteImplementorB : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorB");
+        }
+    } 
+}
