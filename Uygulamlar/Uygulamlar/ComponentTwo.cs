ccb04cc2df6a987f33353be2f1171b7be2129665
adabusra <busraa.adaa@gmail.com>
2017-07-31 14:37:50 +0300

    Component eklendi

---
+++ Uygulamlar/Uygulamlar/ComponentTwo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulamlar
+{
+    class ComponentTwo
+    {
+        static void Main()
+        {
+            KompoziteEleman root = new KompoziteEleman("Picture");
+            root.Ekle(new IlkelEleman("Kırmizi satır"));
+            root.Ekle(new IlkelEleman("Mavi satır"));
+            root.Ekle(new IlkelEleman("Yeşil Kutu"));
+
+            KompoziteEleman comp = new KompoziteEleman("İki Daire");
+            comp.Ekle(new IlkelEleman("Siyah Daire"));
+            comp.Ekle(new IlkelEleman("Beyaz Daire"));
+
+
+
+            root.Ekle(comp);
+
+
+            IlkelEleman ie = new IlkelEleman("Sarı satır");
+            root.Ekle(ie);
+            root.Duzenle(ie);
+
+            root.Ekran(1);
+
+            Console.ReadKey();
+        }
+        
+    }
+
+    abstract class CizimElemani
+    {
+        protected string _ad;
+
+        public CizimElemani(string ad)
+        {
+            this._ad = ad;
+
+        }
+
+        public abstract void Ekle(CizimElemani d);
+        public abstract void Duzenle(CizimElemani d);
+        public abstract void Ekran(int derinlik);
+    }
+
+    class IlkelEleman : CizimElemani
+    {
+        public IlkelEleman(string ad): base(ad)
+        {
+
+        }
+        public override void Ekle(CizimElemani c)
+        {
+            Console.WriteLine("Çizim elemanı eklenemiyor");
+        }
+        public override void Duzenle(CizimElemani c)
+        {
+            Console.WriteLine("Çizim elemanı düzenlenemiyor");
+
+        }
+        public override void Ekran(int derinlik)
+        {
+            Console.WriteLine(new string('-', derinlik) + " " + _ad);
+        }
+    }
+
+    class KompoziteEleman: CizimElemani
+    {
+        private List<CizimElemani> elemanlar = new List<CizimElemani>();
+
+        public KompoziteEleman(string ad): base(ad)
+        {
+        }
+
+        public override void Ekle(CizimElemani d)
+        {
+            elemanlar.Add(d);
+        }
+        public override void Duzenle(CizimElemani d)
+        {
+            elemanlar.Remove(d);
+        }
+        public override void Ekran(int derinlik)
+        {
+            Console.WriteLine(new string('-', derinlik) + "+ " + _ad);
+
+            foreach(CizimElemani d in elemanlar)
+            {
+                d.Ekran(derinlik + 2);
+            }
+        }
+    }
+}
--- Uygulamlar/Uygulamlar/Program.cs
+++ Uygulamlar/Uygulamlar/Program.cs
@@ -8,11 +8,96 @@ namespace Uygulamlar
 {
     class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    Composite root = new Composite("root");
+        //    root.Add(new Leaf("Leaf A"));
+        //    root.Remove(new Leaf("Leaf B"));
+
+        //    Composite comp = new Composite("Composite X");
+        //    comp.Add(new Leaf("Leaf XA"));
+        //    comp.Remove(new Leaf("Leaf XB"));
+
+        //    root.Add(comp);
+        //    root.Add(new Leaf("Leaf C"));
+
+
+        //    Leaf leaf = new Leaf("Leaf D");
+        //    root.Add(leaf);
+        //    root.Remove(leaf);
+
+        //    root.Display(1);
+                 
+
+        //    Console.ReadKey();
+        //}
+    }
+
+    abstract class Component
+    {
+        protected string name;
+
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Display(int depth);
+
+    }
+
+    class Composite : Component
+    {
+        private List<Component> _childeren = new List<Component>();
+
+        public Composite(string name):base(name)
         {
 
-            Console.WriteLine("adabursa");
-            Console.ReadKey();
+        }
+
+        public override void Add(Component component)
+        {
+            _childeren.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            _childeren.Add(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new string('-', depth) + name);
+
+            foreach(Component component in _childeren)
+            {
+                component.Display(depth + 2);
+            }
         }
     }
+
+
+    class Leaf: Component
+    {
+        public Leaf(string name): base(name)
+        {
+
+        }
+        public override void Add(Component c)
+        {
+            Console.WriteLine("Cannot add to leaf");
+        }
+        public override void Remove(Component c)
+        {
+            Console.WriteLine("Cannot remove from a leaf");
+        }
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+
+    }
+
 }
