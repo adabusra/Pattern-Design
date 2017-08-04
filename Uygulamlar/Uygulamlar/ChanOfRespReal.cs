e78423b24bbbd2a14d82c8b7a3ff5a8bbd0fe9e0
adabusra <busraa.adaa@gmail.com>
2017-08-04 12:57:31 +0300

    Chan Of Resp Real

--- Uygulamlar/Uygulamlar/ChanOfRespReal.cs
+++ Uygulamlar/Uygulamlar/ChanOfRespReal.cs
@@ -8,5 +8,114 @@ namespace Uygulamlar
 {
     class ChanOfRespReal
     {
+        static void Main()
+        {
+            Approver larry = new Director();
+            Approver sam = new VicePresident();
+            Approver tammy = new President();
+
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+            
+            Purchase p = new Purchase(2034, 350.00, "Assets");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p);
+
+            p = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p);
+
+            Console.ReadKey();
+        }
     }
+
+    abstract class Approver
+    {
+       protected Approver successor;
+
+       public void SetSuccessor(Approver successor)
+       {
+            this.successor = successor;
+       }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+
+    class Director : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 10000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+
+    class VicePresident : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 25000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+            }
+            else if (successor !=null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+
+    class President : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 10000000.0)
+            {
+                Console.WriteLine("{0} approved request# {1}", this.GetType().Name, purchase.Number);
+            }
+            else
+            {
+                Console.WriteLine("Request# {0} requires an executive meeting", purchase.Number);
+            }
+        }
+    }
+
+    class Purchase
+    {
+        private int _number;
+        private double _amount;
+        private string _purpose;
+
+        public Purchase(int number, double amount, string purpose)
+        {
+            this._number = number;
+            this._amount = amount;
+            this._purpose = purpose;
+        }
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public string Purpose
+        {
+            get { return _purpose; }
+            set { _purpose = value; }
+        }
+
 }
