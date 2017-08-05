86a0c4cb1f52ab099563dec1ce641b7344adcf3d
adabusra <busraa.adaa@gmail.com>
2017-08-05 19:44:01 +0300

    Interpreter

---
+++ Uygulamlar/Uygulamlar/Interpreter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulamlar
+{
+    class Interpreter
+    {
+        
+        static void Main()
+        {
+            Context context = new Context();
+ 
+            ArrayList list = new ArrayList();
+            
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+      
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+    class Context
+    {
+    }
+    
+    abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+    
+    class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Terminal.Interpret()");
+        }
+    }
+    
+    class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Nonterminal.Interpret()");
+        }
+    }
+}
