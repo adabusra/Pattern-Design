ef33ca2e2e0571afe96d34e224f2622186ebc4d5
adabusra <busraa.adaa@gmail.com>
2017-08-16 21:17:22 +0300

    State

--- Uygulamlar/Uygulamlar/ObserverReal.cs
+++ Uygulamlar/Uygulamlar/ObserverReal.cs
@@ -8,21 +8,21 @@ namespace Uygulamlar
 {
     class ObserverReal
     {
-        static void Main()
-        {
-            IBM ibm = new IBM("IBM", 120.00);
-            ibm.Attach(new Investor("Sorros"));
-            ibm.Attach(new Investor("Berkshire"));
+        //static void Main()
+        //{
+        //    IBM ibm = new IBM("IBM", 120.00);
+        //    ibm.Attach(new Investor("Sorros"));
+        //    ibm.Attach(new Investor("Berkshire"));
 
-            ibm.Price = 120.10;
-            ibm.Price = 121.00;
-            ibm.Price = 120.50;
-            ibm.Price = 120.75;
+        //    ibm.Price = 120.10;
+        //    ibm.Price = 121.00;
+        //    ibm.Price = 120.50;
+        //    ibm.Price = 120.75;
 
 
-            Console.ReadKey();
+        //    Console.ReadKey();
 
-        }
+        //}
     }
 
     abstract class Stock
---
+++ Uygulamlar/Uygulamlar/State.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulamlar
+{
+    class StateApp
+    {
+        static void Main()
+        {
+            ContextState c = new ContextState(new ConcreteStateA());
+
+
+            c.Request();
+            c.Request();
+            c.Request();
+            c.Request();
+
+
+            Console.ReadKey();
+        }
+
+    }
+
+    abstract class State
+    {
+        public abstract void Handle(ContextState context);
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle(ContextState context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    class ConcreteStateB: State
+    {
+        public override void Handle(ContextState context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    class ContextState
+    {
+        private State _state;
+
+        public ContextState(State state)
+        {
+            this._state = state;
+        }
+
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State: " + _state.GetType().Name);
+            }
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+    }
+}
